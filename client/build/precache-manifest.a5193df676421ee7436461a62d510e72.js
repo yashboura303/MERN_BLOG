b1b49f536ce7513e196bc5149a4d669250501c59
yash <yashboura303@gmail.com>
2020-05-31 01:13:17 +0530

    heroku changes

---
+++ client/build/precache-manifest.a5193df676421ee7436461a62d510e72.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577c82c7b84bd4b788c652cf5ccf039b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "23f652960f1ca10f0d2a",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fae9e8fb68c39607fe00",
+    "url": "./static/css/main.5803987b.chunk.css"
+  },
+  {
+    "revision": "23f652960f1ca10f0d2a",
+    "url": "./static/js/2.1eb9a426.chunk.js"
+  },
+  {
+    "revision": "14060d5403b5cb599a71704f7b391b06",
+    "url": "./static/js/2.1eb9a426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fae9e8fb68c39607fe00",
+    "url": "./static/js/main.74c2f428.chunk.js"
+  },
+  {
+    "revision": "27bc183d629a888a1e51",
+    "url": "./static/js/runtime-main.9a5e869b.js"
+  },
+  {
+    "revision": "66adbeab59a36d78ce927ec507a8a593",
+    "url": "./static/media/blog.66adbeab.svg"
+  },
+  {
+    "revision": "630c11e4d4febac4cc08e67cd2213d29",
+    "url": "./static/media/date.630c11e4.svg"
+  },
+  {
+    "revision": "e8fef7e5b142469d5ee6fed872f0ee5a",
+    "url": "./static/media/heart.e8fef7e5.svg"
+  },
+  {
+    "revision": "14ce8ba42bf0e7f712acfc35a23f68d7",
+    "url": "./static/media/liked.14ce8ba4.svg"
+  },
+  {
+    "revision": "d1e3d0c158177513cad5544491770059",
+    "url": "./static/media/not-liked.d1e3d0c1.svg"
+  }
+]);
