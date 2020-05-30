fad577e5a85451fdbd1c6a09b54b5158d4aad217
yash <yashboura303@gmail.com>
2020-05-31 00:01:33 +0530

    heroku changes

--- app.js
+++ app.js
@@ -1,46 +1,56 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const dotenv = require('dotenv');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const path = require("path");
+const dotenv = require("dotenv");
 dotenv.config();
-
 
 var app = express();
 app.use(cors());
 
 const mongoDB = process.env.MONGO_ATLAS_KEY;
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true , useCreateIndex:true});
+mongoose.connect(mongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-app.use(logger('dev'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json({ error: err });
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.json({ error: err });
 });
 
 module.exports = app;
